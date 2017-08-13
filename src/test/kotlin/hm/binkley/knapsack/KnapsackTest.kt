12ae004fbe7569a28ab66db772dbe0f51f43c13e
Brian Oxley <boxley@thoughtworks.com>
2017-08-13 13:32:25 -0500

    Raise test coverage

--- src/test/kotlin/hm/binkley/knapsack/KnapsackTest.kt
+++ src/test/kotlin/hm/binkley/knapsack/KnapsackTest.kt
@@ -1,8 +1,26 @@
 package hm.binkley.knapsack
 
+import org.junit.Before
 import org.junit.Test
 
 internal class KnapsackTest {
+    private lateinit var knapsack: Knapsack
+
+    @Before
+    fun setUpFixture() {
+        knapsack = Knapsack()
+    }
+
     @Test
-    fun shouldStartEmpty() = Knapsack().isEmpty() `is` true
+    fun shouldStartEmpty() = knapsack.isEmpty() `is` true
+
+    @Test
+    fun shouldStartUnableToFindKeys()
+            = knapsack.containsKey("foo") `is` false
+
+    @Test
+    fun shouldStartUnableToFetchValues() = knapsack["foo"] `is` null
+
+    @Test
+    fun shouldFetchDefaultValue() = knapsack.getOrDefault("foo", 3) `is` 3
 }
