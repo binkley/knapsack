94282ae5ebde1476f876a333b12881bcb2ff3da9
Brian Oxley <boxley@thoughtworks.com>
2017-08-14 06:29:58 -0400

    Drop Exposed

--- src/main/kotlin/hm/binkley/knapsack/Knapsack.kt
+++ src/main/kotlin/hm/binkley/knapsack/Knapsack.kt
@@ -1,12 +1,5 @@
 package hm.binkley.knapsack
 
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils.create
-import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.Transaction
-import org.jetbrains.exposed.sql.transactions.transaction
-import org.sqlite.SQLiteDataSource
-import java.sql.Connection.TRANSACTION_SERIALIZABLE
 import kotlin.collections.MutableMap.MutableEntry
 
 class Knapsack : AbstractMutableMap<String, Any>() {
@@ -16,19 +9,4 @@ class Knapsack : AbstractMutableMap<String, Any>() {
 
     override val entries: MutableSet<MutableEntry<String, Any>>
         get() = delegate.entries
-
-    fun <T> execute(statement: Transaction.() -> T): T {
-        val dataSource = SQLiteDataSource()
-        dataSource.url = "jdbc:sqlite::memory:"
-        Database.connect(dataSource)
-        return transaction(TRANSACTION_SERIALIZABLE, 0) {
-            create(Layer)
-            statement.invoke(this)
-        }
-    }
-
-    object Layer : Table() {
-        val key = text("key").primaryKey()
-        val value = text("value")
-    }
 }
