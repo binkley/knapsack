d4e2c4687ad0d265da9204d82c3f0431fcd334e1
Brian Oxley <boxley@thoughtworks.com>
2017-08-27 08:32:10 -0400

    Cleaner

--- src/applicationTest/kotlin/hm/binkley/knapsack/KnapsackDatabaseTestRule.kt
+++ src/applicationTest/kotlin/hm/binkley/knapsack/KnapsackDatabaseTestRule.kt
@@ -1,12 +1,10 @@
 package hm.binkley.knapsack
 
 import org.junit.rules.ExternalResource
-import java.sql.Connection
 import java.sql.Connection.TRANSACTION_SERIALIZABLE
 import java.sql.DriverManager.getConnection
 
 class KnapsackDatabaseTestRule : ExternalResource() {
-    private lateinit var _database: Connection
     private lateinit var _loader: SQLLoader
 
     val loader
@@ -20,9 +18,10 @@ class KnapsackDatabaseTestRule : ExternalResource() {
 
     override fun before() {
         val knapsackDir = System.getProperty("java.io.tmpdir")
-        _database = getConnection("jdbc:hsqldb:file:$knapsackDir/knapsack/db")
-        _database.transactionIsolation = TRANSACTION_SERIALIZABLE
-        _loader = SQLLoader(_database)
+        val database = getConnection(
+                "jdbc:hsqldb:file:$knapsackDir/knapsack/db")
+        database.transactionIsolation = TRANSACTION_SERIALIZABLE
+        _loader = SQLLoader(database)
         _loader.loadSchema()
     }
 
