8c5c71bbf60914201223efaa1ce09f91e1d4b456
Brian Oxley <boxley@thoughtworks.com>
2017-08-28 06:50:28 -0400

    Look at alternative knapsack dirs

--- src/main/kotlin/hm/binkley/knapsack/Database.kt
+++ src/main/kotlin/hm/binkley/knapsack/Database.kt
@@ -6,13 +6,14 @@ import java.sql.DriverManager.getConnection
 
 class Database(private val database: Connection) : Connection by database {
     companion object {
-        fun main(): Database = database(System.getProperty("user.home"))
+        fun main(): Database = database(
+                "${System.getProperty("user.home")}/.knapsack")
 
-        fun test(): Database = database(System.getProperty("java.io.tmpdir"))
+        fun test(): Database = database(
+                "${System.getProperty("java.io.tmpdir")}/knapsack")
 
         private fun database(knapsackDir: String): Database {
-            val connection = getConnection(
-                    "jdbc:hsqldb:file:$knapsackDir/knapsack/db")
+            val connection = getConnection("jdbc:hsqldb:file:$knapsackDir")
             connection.transactionIsolation = TRANSACTION_SERIALIZABLE
             return Database(connection)
         }
