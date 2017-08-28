c3710cb85502354188a5735e8ff70b0cf5f7c6b2
Brian Oxley <boxley@thoughtworks.com>
2017-08-28 06:47:35 -0400

    Formatting

--- src/main/kotlin/hm/binkley/knapsack/Database.kt
+++ src/main/kotlin/hm/binkley/knapsack/Database.kt
@@ -1,19 +1,19 @@
 package hm.binkley.knapsack
 
 import java.sql.Connection
+import java.sql.Connection.TRANSACTION_SERIALIZABLE
 import java.sql.DriverManager.getConnection
 
 class Database(private val database: Connection) : Connection by database {
     companion object {
         fun main(): Database = database(System.getProperty("user.home"))
 
-        fun test(): Database =
-                database(System.getProperty("java.io.tmpdir"))
+        fun test(): Database = database(System.getProperty("java.io.tmpdir"))
 
         private fun database(knapsackDir: String): Database {
             val connection = getConnection(
                     "jdbc:hsqldb:file:$knapsackDir/knapsack/db")
-            connection.transactionIsolation = Connection.TRANSACTION_SERIALIZABLE
+            connection.transactionIsolation = TRANSACTION_SERIALIZABLE
             return Database(connection)
         }
     }
