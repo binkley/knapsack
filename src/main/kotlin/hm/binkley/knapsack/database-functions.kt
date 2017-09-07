8d7d2e680734ccc7f6d2681167b4cf993a6440aa
Brian Oxley <boxley@thoughtworks.com>
2017-09-07 07:35:04 -0400

    Wrong thinking on map-list
    It should be a list, not a map view of a list of maps.

--- src/main/kotlin/hm/binkley/knapsack/database-functions.kt
+++ src/main/kotlin/hm/binkley/knapsack/database-functions.kt
@@ -7,5 +7,3 @@ fun Database.entryIterator(layer: Int) = DatabaseEntryIterator(this, layer)
 fun Database.set(layer: Int) = DatabaseSet(this, layer)
 
 fun Database.map(layer: Int) = DatabaseMap(this, layer)
-
-fun Database.mapList() = DatabaseMapList(this)
