0f89be118cec8d3b55f1b0bb494aaafdb5de0d69
Brian Oxley <boxley@thoughtworks.com>
2017-09-18 16:56:54 -0500

    Pull up general result set iterator

--- src/mockTest/kotlin/hm/binkley/knapsack/DatabaseEntryIteratorMockTest.kt
+++ src/mockTest/kotlin/hm/binkley/knapsack/DatabaseEntryIteratorMockTest.kt
@@ -10,6 +10,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 import java.sql.ResultSet
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -20,14 +21,19 @@ internal class DatabaseEntryIteratorMockTest {
         on { selectMapKeys(0) } doReturn selectKeysResults
     }
 
+    private val iter = database.entryIterator(0)
+
+    @Test
+    fun shouldHaveLayer_forJaCoCo() {
+        assertEquals(iter.layer, 0)
+    }
+
     @Test
     fun shouldClose() {
         iter.use {}
 
         verify(selectKeysResults, atLeastOnce()).close()
     }
-
-    private val iter = database.entryIterator(0)
 
     @Test
     fun shouldHasNext() {
