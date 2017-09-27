6d89edf59a6929e23a8afe71aaf4c0836e439bda
Brian Oxley <boxley@thoughtworks.com>
2017-09-27 14:37:00 -0500

    Hygiene

--- src/main/kotlin/hm/binkley/knapsack/SQLReader.kt
+++ src/main/kotlin/hm/binkley/knapsack/SQLReader.kt
@@ -1,50 +1,16 @@
 package hm.binkley.knapsack
 
 import java.io.FileNotFoundException
+import java.net.URL
 import java.util.ArrayList
 import java.util.regex.Pattern
 
 class SQLReader(private val purpose: String) {
-    fun lines() = catenateSql(rawLines)
+    fun lines() = catenateSql(purpose, rawLines())
 
     fun oneLine() = lines().single()
 
-    private fun catenateSql(rawLines: List<String>): List<String> {
-        val lines = ArrayList<String>()
-        val buffer = StringBuilder()
-        for (rawLine in rawLines.
-                map { it.trim() }.
-                filter { !it.isEmpty() }.
-                filter { !it.isComment() }) {
-            buffer += ' '
-
-            val matcher = rawLine.matchEnd()
-            if (!matcher.find()) {
-                buffer += rawLine
-                continue
-            }
-
-            buffer += rawLine.substring(0, matcher.start())
-            lines.add(buffer.trim().toString())
-            buffer.setLength(0)
-        }
-
-        if (buffer.isNotEmpty())
-            throw IllegalArgumentException(
-                    "Unterminated SQL in: $purpose: $buffer")
-
-        return lines
-    }
-
-    private val rawLines: List<String>
-        get() {
-            val source = "/hm/binkley/knapsack/knapsack-$purpose.sql"
-            val resource = javaClass.getResource(source)
-                    ?: throw FileNotFoundException(source)
-            return resource.
-                    readText().
-                    lines()
-        }
+    private fun rawLines() = source(purpose).readText().lines()
 
     companion object {
         private val TERMINATING_SEMICOLON = Pattern.compile("\\s*;\\s*$")
@@ -55,6 +21,40 @@ class SQLReader(private val purpose: String) {
 
         private infix operator fun <T> StringBuilder.plusAssign(text: T) {
             append(text)
+        }
+
+        private fun source(purpose: String): URL {
+            val source = "/hm/binkley/knapsack/knapsack-$purpose.sql"
+            return javaClass.getResource(source)
+                    ?: throw FileNotFoundException(source)
+        }
+
+        private fun catenateSql(purpose: String, rawLines: List<String>):
+                List<String> {
+            val lines = ArrayList<String>()
+            val buffer = StringBuilder()
+            for (line in rawLines.
+                    map { it.trim() }.
+                    filter { !it.isEmpty() }.
+                    filter { !it.isComment() }) {
+                buffer += ' '
+
+                val matcher = line.matchEnd()
+                if (!matcher.find()) {
+                    buffer += line
+                    continue
+                }
+
+                buffer += line.substring(0, matcher.start())
+                lines.add(buffer.trim().toString())
+                buffer.setLength(0)
+            }
+
+            if (buffer.isEmpty())
+                return lines
+
+            throw IllegalArgumentException(
+                    "Unterminated SQL (no ending semicolon) in: $purpose: $buffer")
         }
     }
 }
